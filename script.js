009b14a0cf04ee54b50e6ee4b76ac0564d91e356
lojain <lojainnasr039@gmail.com>
2023-12-30 18:25:43 +0300

    update js

--- script.js
+++ script.js
@@ -3,14 +3,13 @@ function saveRating(rating) {
 }
 
 document.getElementById('submit').addEventListener('click', function() {
-    let rating = localStorage.getItem('rating');
-if (rating) {
         window.location.href = 'thank.html';
-        
-        let span = document.querySelector('#rating');
-        span.innerHTML = `${rating}`;
-    } else {
-        alert('Please rate before submitting.');
-    }
-
 });
+function displayRating() {
+let rating = localStorage.getItem('rating');
+
+if (rating !== null){
+let span = document.querySelector('#rating');
+span.innerHTML = `${rating}`;
+console.log(span);
+        }}
